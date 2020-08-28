ebb1be478bad52e5d909f6c8cf67b13e10c0f0bb
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2020-08-29 01:00:27 +0900

    Add plugin entry point

--- unity-native-plugin/src/interface.rs
+++ unity-native-plugin/src/interface.rs
@@ -5,12 +5,33 @@ pub trait UnityInterface {
     fn new(interface: *const IUnityInterface) -> Self;
 }
 
+static mut UNITY_INTERFACES: Option<UnityInterfaces> = None;
+
 pub struct UnityInterfaces {
     interfaces: *const IUnityInterfaces,
 }
 
 impl UnityInterfaces {
-    fn get_interface<T: UnityInterface>(&self) -> Option<T> {
+    pub fn get_unity_interfaces() -> &UnityInterfaces {
+        unsafe {
+            UNITY_INTERFACES.as_ref().unwrap()
+        }
+    }
+
+    pub(crate) fn set_native_unity_interfaces(
+        interfaces: Option<*const unity_native_plugin_sys::IUnityInterfaces>,
+    ) {
+        unsafe {
+            UNITY_INTERFACES = match interfaces {
+                Some(interfaces) => Some(UnityInterfaces {
+                    interfaces: interfaces,
+                }),
+                None => None,
+            }
+        }
+    }
+
+    pub fn get_interface<T: UnityInterface>(&self) -> Option<T> {
         unsafe {
             if let Some(intf) = &(*self.interfaces).GetInterface {
                 let r = intf(T::get_interface_guid());
--- unity-native-plugin/src/lib.rs
+++ unity-native-plugin/src/lib.rs
@@ -1,4 +1,17 @@
+use crate::interface::UnityInterfaces;
+
 pub mod enums;
 pub mod graphics;
 pub mod interface;
 
+#[no_mangle]
+#[allow(non_snake_case)]
+extern "system" fn UnityPluginLoad(interfaces: Option::<*const unity_native_plugin_sys::IUnityInterfaces> ) {
+    interface::UnityInterfaces::set_native_unity_interfaces(interfaces);
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+extern "system" fn UnityPluginUnload() {
+    interface::UnityInterfaces::set_native_unity_interfaces(None);
+}
