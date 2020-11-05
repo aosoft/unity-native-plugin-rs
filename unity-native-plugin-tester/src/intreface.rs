786de444e8698ab42934d473d6c5bbf46369fd52
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2020-11-06 01:13:09 +0900

    Add unity-native-plugin-tester/interface.rs

---
+++ unity-native-plugin-tester/src/intreface.rs
@@ -0,0 +1,61 @@
+use std::collections::HashMap;
+use std::os::raw::c_ulonglong;
+use unity_native_plugin_sys::*;
+
+#[derive(Default, Copy, Clone, Eq, PartialEq, Hash)]
+pub struct InfKey {
+    pub high: c_ulonglong,
+    pub low: c_ulonglong,
+}
+
+type InfMap = HashMap<InfKey, *mut IUnityInterface>;
+
+static mut INTERFACES: Option<InfMap> = None;
+
+extern "system" fn get_interface(guid: UnityInterfaceGUID) -> *mut IUnityInterface {
+    get_interface_split(guid.m_GUIDHigh, guid.m_GUIDLow)
+}
+
+extern "system" fn register_interface(guid: UnityInterfaceGUID, ptr: *mut IUnityInterface) {
+    register_interface_split(guid.m_GUIDHigh, guid.m_GUIDLow, ptr);
+}
+
+extern "system" fn get_interface_split(
+    high: ::std::os::raw::c_ulonglong,
+    low: ::std::os::raw::c_ulonglong,
+) -> *mut IUnityInterface {
+    unsafe {
+        if let Some(infs) = &INTERFACES {
+            *infs.get(&InfKey { high, low }).unwrap_or(&std::ptr::null_mut())
+        } else {
+            std::ptr::null_mut()
+        }
+    }
+}
+
+extern "system" fn register_interface_split(
+    high: ::std::os::raw::c_ulonglong,
+    low: ::std::os::raw::c_ulonglong,
+    ptr: *mut IUnityInterface,
+) {
+    unsafe {
+        if let Some(infs) = &mut INTERFACES {
+            infs.insert(InfKey { high, low }, ptr);
+        }
+    }
+}
+
+pub fn get_interfaces() -> IUnityInterfaces {
+    unsafe {
+        if INTERFACES.is_none() {
+            INTERFACES = Some(InfMap::new());
+        }
+    }
+
+    IUnityInterfaces {
+        GetInterface: Some(get_interface),
+        RegisterInterface: Some(register_interface),
+        GetInterfaceSplit: Some(get_interface_split),
+        RegisterInterfaceSplit: Some(register_interface_split),
+    }
+}
--- unity-native-plugin-tester/src/lib.rs
+++ unity-native-plugin-tester/src/lib.rs
@@ -1,4 +1,7 @@
 #[cfg(test)]
+pub mod intreface;
+
+#[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
