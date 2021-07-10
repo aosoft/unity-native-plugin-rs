9c275a8ff181dacdf9b7816cd66e55d5f9d58479
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2021-07-11 00:25:28 +0900

    Fix compile error * Apply rustfmt

--- unity-native-plugin/src/d3d12.rs
+++ unity-native-plugin/src/d3d12.rs
@@ -263,9 +263,9 @@ impl UnityGraphicsD3D12v5 {
         self.interface().GetCommandQueue.expect("GetCommandQueue")() as ComPtr
     }
 
-    pub unsafe fn texture_from_render_buffer(&self, buffer: *mut graphics::RenderBuffer) -> ComPtr {
+    pub unsafe fn texture_from_render_buffer(&self, rb: graphics::RenderBuffer) -> ComPtr {
         self.interface()
             .TextureFromRenderBuffer
-            .expect("TextureFromRenderBuffer")(buffer) as ComPtr
+            .expect("TextureFromRenderBuffer")(rb) as ComPtr
     }
 }
