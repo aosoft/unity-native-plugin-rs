65d00c9215de6126772d0250ff750d5b0c6f41d3
ruccho <ruccho@ruccho.com>
2025-11-05 11:50:13 +0900

    comments

--- unity-native-plugin-sys/bindgen_metal.sh
+++ unity-native-plugin-sys/bindgen_metal.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Output of bindgen for Objective-C depends on objc package so we need to exclude objc-dependent parts by hand.
+# Output of bindgen for Objective-C depends on objc so we need to exclude objc-dependent parts by hand.
 
 bindgen \
-  --output ./src/metal.rs \
+  --output ./src/metal_api.rs \
   --with-derive-default \
   --no-derive-debug \
   --blocklist-file ./include/IUnityInterface.h \
--- unity-native-plugin-sys/src/metal.rs
+++ unity-native-plugin-sys/src/metal.rs
@@ -1,4 +1,5 @@
 
+// facades for objc-dependent types
 type NSBundle = *mut u8;
 type MTLRenderPassDescriptor = *mut u8;
 
