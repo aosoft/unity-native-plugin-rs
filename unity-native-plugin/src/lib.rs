f1bf504f6762b0b4903edf6d5cbb4a41b3ec0566
ruccho <ruccho@ruccho.com>
2026-01-30 11:54:11 +0900

    reviews

--- unity-native-plugin/src/lib.rs
+++ unity-native-plugin/src/lib.rs
@@ -4,7 +4,7 @@ pub mod d3d11;
 #[cfg(feature = "d3d12")]
 pub mod d3d12;
 
-#[cfg(all(feature = "metal_objc2", target_vendor = "apple"))]
+#[cfg(all(feature = "metal", target_vendor = "apple"))]
 pub mod metal;
 
 #[cfg(feature = "profiler")]
