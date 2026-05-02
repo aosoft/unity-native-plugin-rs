4c1477c43ca3ab0426ea9c309d2fbb777c224e23
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2026-05-03 01:14:52 +0900

    Regenerate Metal API bindings using `bindgen` and replace `objc` types with raw pointers for compatibility.

--- unity-native-plugin-sys/bindgen_metal.sh
+++ unity-native-plugin-sys/bindgen_metal.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-# Output of bindgen for Objective-C depends on objc so we need to exclude objc-dependent parts by hand.
-
 bindgen \
   --output ./src/metal_api.rs \
   --with-derive-default \
   --no-derive-debug \
-  --blocklist-file ./include/IUnityInterface.h \
-  ./include/IUnityGraphicsMetal.h -- -x objective-c
+  --blocklist-type "NSBundle|MTLRenderPassDescriptor|INSBundle|IMTLRenderPassDescriptor" \
+  --raw-line "pub type NSBundle = *mut u8;" \
+  --raw-line "pub type MTLRenderPassDescriptor = *mut u8;" \
+  wrapper.hpp -- -x objective-c++ -I ./include -I $VULKAN_SDK/Include
+sed -i -e "s/extern \"C\"/extern \"system\"/g" ./src/metal_api.rs
